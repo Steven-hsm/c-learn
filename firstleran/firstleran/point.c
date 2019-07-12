fc3e605d71b77ff50e9544d5725ef891e5fca33d
黄森明 <1586558083@qq.com>
2019-07-12 17:13:59 +0800

    指针

---
+++ firstleran/firstleran/point.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int a = 0;
+	int b = 0;
+}
+void swap(int a, int b) {
+
+}
---
+++ firstleran/firstleran/readme.md
@@ -0,0 +1,5 @@
+# include "文件名" 
+# define 名字
+#if !define (hdr)
+define hdr
+#end if
